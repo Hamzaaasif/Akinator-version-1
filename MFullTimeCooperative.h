201f54ff65e8f270c0083a57f817a56757e7d66f
Hamza Asif <hamzaasif0987@gmail.com>
2018-04-25 19:27:30 +0500

    working properly

--- MFullTimeCooperative.h
+++ MFullTimeCooperative.h
@@ -6,10 +6,6 @@ class FullTimeCooperative
   public:
 void MFullTimeCooperative() 
 {
-  cout<<"Is your teacher Full Time Cooperative?"<<endl;
-  cin>>this->ans;
-  if(ans == "Yes" || ans == "yes")
-  {
   cout<<"Is your teacher teaches related to statistics and probability?"<<endl;
   cin>>this->ans;
   if(this->ans == "Yes" || ans == "yes")
@@ -27,11 +23,6 @@ void MFullTimeCooperative()
   else 
   {
     cout<<"Your teacher is MR S. MEESAM ALI ZAIDI"<<endl;
-  }
- }
-  else
-  {
-    cout<<"Apna baap ka naam chahta hy kya?"<<endl;
   }
 
   end:;
